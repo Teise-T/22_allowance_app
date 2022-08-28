9c490b3d509fa6a18151b1d621e7c92c83ca9cd1
teise <teisteu@gmail.com>
2022-08-29 10:11:38 +1200

    Main page GUI init

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -9,14 +9,15 @@ class AllowanceGUI:
     def __init__(self):
         root = Tk()
         self.style = ttk.Style()
-
+        self.allowance_string = ttk.StringVar(root, "Allowance app")
         self.style.configure('allowance', background="#f9f9f9ff")
-        self.allowance = ttk.Label(root, text="Allowance app")
+        self.allowance = ttk.Label(root, textvariable=self.allowance_string)
         self.allowance.grid(column=0, row=0, columnspan=3, rowspan=6, pady=10, padx=10)
         self.Font.tuple = (Comfortaa, )
-        Desired_font = tkinter.font.Font(family="Comic Sans MS",
+        self.Comfortaa = tkinter.font.Font(family="Comfortaa",
                                          size=20,
                                          weight="bold")
+
         root.title("Allowance app")
         root.configure(background='lavender')
         root.mainloop()
