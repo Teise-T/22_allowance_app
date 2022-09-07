c675d882539c0e83778b7404fde7fb115aa39575
teise <teisteu@gmail.com>
2022-09-08 11:46:01 +1200

    Main page GUI changes, font added

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -1,23 +1,35 @@
 from tkinter import *
 from tkinter import ttk
-from tkinter import tkinter.font
-
+# from tkinter import tkinter.font
+from matplotlib import font_manager
 import random
+
+font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
+
+
 
 
 class AllowanceGUI:
     def __init__(self):
         root = Tk()
         self.style = ttk.Style()
-        self.allowance_string = ttk.StringVar(root, "Allowance app")
         self.style.configure('allowance', background="#f9f9f9ff")
-        self.allowance = ttk.Label(root, textvariable=self.allowance_string)
-        self.allowance.grid(column=0, row=0, columnspan=3, rowspan=6, pady=10, padx=10)
-        self.Font.tuple = (Comfortaa, )
-        self.Comfortaa = tkinter.font.Font(family="Comfortaa",
-                                         size=20,
-                                         weight="bold")
+        self.style.configure('frame1', background="#f9f9f9ff")
+        self.frame1 = ttk.Frame(root)
+        self.frame1.grid(columnspan=40, rowspan=50)
+        self.label1 = ttk.Label(self.frame1, text="Ranui Moni", font="Comfortaa")
+        self.label1.grid(column=0, row=0, pady=20, padx=20, sticky="NSEW")
+        self.buttonFrame = ttk.Frame(root)
+        self.nButton = ttk.Button(self.)
+        self.label2.grid(column=0, row=5, )
 
+        # self.Font.tuple = (Comfortaa, )
+        # self.Comfortaa = tkinter.font.Font(family="Comfortaa",
+        # size=20,
+        # weight="bold")
         root.title("Allowance app")
         root.configure(background='lavender')
         root.mainloop()
+
+
+AllowanceGUI()
