0640867f1814212d95006dc1dc772c3a00ad493c
teise <teisteu@gmail.com>
2022-09-10 15:26:41 +1200

    bg is all same colour now

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -7,28 +7,23 @@ import random
 font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
 
 
-
-
 class AllowanceGUI:
     def __init__(self):
         root = Tk()
+        root.geometry("350x500")
         self.style = ttk.Style()
         self.style.configure('allowance', background="#f9f9f9ff")
         self.style.configure('frame1', background="#f9f9f9ff")
         self.frame1 = ttk.Frame(root)
-        self.frame1.grid(columnspan=40, rowspan=50)
+        self.frame1.grid(column=, row=0, columnspan=4, rowspan=10, sticky="NSEW")
         self.label1 = ttk.Label(self.frame1, text="Ranui Moni", font="Comfortaa")
-        self.label1.grid(column=0, row=0, pady=20, padx=20, sticky="NSEW")
-        self.buttonFrame = ttk.Frame(root)
-        self.nButton = ttk.Button(self.)
-        self.label2.grid(column=0, row=5, )
-
-        # self.Font.tuple = (Comfortaa, )
-        # self.Comfortaa = tkinter.font.Font(family="Comfortaa",
-        # size=20,
-        # weight="bold")
+        self.label1.grid(column=2, row=0, pady=20, padx=20, sticky="NSEW")
+        self.nButton = ttk.Button(self.frame1)
+        self.nButton.grid(column=1)
+        self.label2 = ttk.Label(self.frame1, text="label2")
+        self.label2.grid(column=0, row=1)
         root.title("Allowance app")
-        root.configure(background='lavender')
+        root.configure()
         root.mainloop()
 
 
