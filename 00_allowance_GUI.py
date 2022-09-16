d76e29c48b2432fd25ba8c3bd7a842c21b1a5b5f
teise <teisteu@gmail.com>
2022-09-16 13:43:28 +1200

    update

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -15,7 +15,7 @@ class AllowanceGUI:
         self.style.configure('allowance', background="#f9f9f9ff")
         self.style.configure('frame1', background="#f9f9f9ff")
         self.frame1 = ttk.Frame(root)
-        self.frame1.grid(column=, row=0, columnspan=4, rowspan=10, sticky="NSEW")
+        self.frame1.grid(column=0, row=0, columnspan=4, rowspan=10, sticky="NSEW")
         self.label1 = ttk.Label(self.frame1, text="Ranui Moni", font="Comfortaa")
         self.label1.grid(column=2, row=0, pady=20, padx=20, sticky="NSEW")
         self.nButton = ttk.Button(self.frame1)
