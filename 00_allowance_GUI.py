1a325869cf771afc031ba3574b9429e222f01c75
teise <teisteu@gmail.com>
2022-09-27 17:09:03 +1300

    logo on page and title font altered

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -4,9 +4,6 @@ from tkinter import ttk
 from matplotlib import font_manager
 
 from PIL import Image, ImageTk
-
-img = ImageTk.PhotoImage(Image.open("growth.png"))
-
 font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
 
 
@@ -14,16 +11,27 @@ class AllowanceGUI:
     def __init__(self):
         root = Tk()
         root.geometry("350x500")
-        self.logo = PhotoImage(file="growth.png")
+
+        self.logo = PhotoImage(file=r"growth.png")
+        self.logo = self.logo.subsample(7, 7)
+
+        root.iconphoto(False, self.logo)
+
         self.style = ttk.Style()
         self.style.configure('allowance', background="#f9f9f9ff")
         self.style.configure('frame1', background="#f9f9f9ff")
+        self.style.configure('frame1_header', )
+
         self.frame1 = ttk.Frame(root)
-        self.frame1.grid(column=0, row=0, columnspan=4, rowspan=10, sticky="NSEW")
-        self.label1 = ttk.Label(self.frame1, text="Ranui Moni", font="Comfortaa")
-        self.label1.grid(column=2, row=0, pady=20, padx=20, sticky="NSEW")
+        self.frame1_logo = ttk.Label(self.frame1, image=self.logo)
+        self.frame1_header = ttk.Label(self.frame1, text="Ranui Moni", font=('Comfortaa', 20))
         self.label2 = ttk.Label(self.frame1)
+
+        self.frame1.grid(column=2, row=2, sticky="NSEW")
+        self.frame1_logo.grid(column=1, row=0, pady=5, padx=(350 / 2 - 40), sticky="NSEW")
+        self.frame1_header.grid(column=1, row=1, pady=0, padx=(350 / 2 - 80), sticky="NSEW")
         self.label2.grid(column=0, row=1)
+
         root.title("Allowance app")
         root.configure()
         root.mainloop()
