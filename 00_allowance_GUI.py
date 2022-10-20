a8a2b12cbe5029884761122fea01a0f6c056787e
teise <teisteu@gmail.com>
2022-10-21 10:39:21 +1300

    1st attempt at adding a new window function

--- 00_allowance_GUI.py
+++ 00_allowance_GUI.py
@@ -7,69 +7,97 @@ font_manager.findSystemFonts(fontpaths=None, fontext="ttf")
 
 
 # Allowance class
+class NewWindow:
+    pass
+
+
 class AllowanceGUI:
     def __init__(self):
         super().__init__()
+
+        app_width = 350
+        app_height = 600
+
+        resolution = str(app_width) + "x" + str(app_height)
+
         root = Tk()
-        root.geometry("350x500")
+        root.geometry(resolution)
+
+        # puts image in the corner of the page
+        logo = PhotoImage(file=r"growth.png")
+        logo = logo.subsample(7, 7)
+        root.iconphoto(False, logo)
+
+        # makes help icon show up on GUI
+        help = PhotoImage(file=r"help (1).png").subsample(2, 2)
+
+        # adding style to elements on the GUI
+        style = ttk.Style()
+        style.configure('allowance', background="#f9f9f9ff")
+        style.configure('frame1', background="#f9f9f9ff")
+        style.configure('frame1_header', )
+
+        outer_frame = ttk.Frame(root, width=app_width, height=app_height)
+        outer_frame.grid(column=0, row=0)
+
+        # Frame 1, encompasses the entire page
+        frame1 = ttk.Frame(outer_frame)
+        frame1.grid(column=0, row=1, sticky="NSEW", pady=10)
+        # A separate frame for the help icon
+        frame1_logo = ttk.Label(frame1, image=logo)
+        frame1_header = ttk.Label(frame1, text="Ranui Moni", font=('Comfortaa', 20))  # A separate frame for the header
+        frame1_logo.grid(column=2, row=0, pady=0, padx=0, sticky="NSEW")
+        frame1_header.grid(column=2, row=1, pady=0, padx=0, sticky="NSEW")
+
+        frame2 = ttk.Frame(outer_frame)
+        frame2.grid()
+
+        # Creating a photoImage object to use image
+        nikaupho = PhotoImage(file=r"jellyfish.png")
+        hanapho = PhotoImage(file=r"fox.png")
+        tiapho = PhotoImage(file=r"whale.png")
+
+        # Resizing image to fit on button
+        nikaupho = nikaupho.subsample(4, 4)
+        hanapho = hanapho.subsample(4, 4)
+        tiapho = tiapho.subsample(4, 4)
+
+    def openNewWindow():
+        # Toplevel object which will
+        # be treated as a new window
+        newWindow = Toplevel(root)
+
+        # sets the title of the
+        # Toplevel widget
+        newWindow.title("New Window")
+
+        # sets the geometry of toplevel
+        newWindow.geometry("200x200")
+
+        # A Label widget to show in toplevel
+        Label(newWindow,
+              text="This is a new window").pack()
+
+    label = ttk.Label(root,
+                  text="This is the main window")
+
+    label.pack(pady=10)
+    # This function enables the button to take the user to another page e.g help page
+    def Nikau(event, frame2=None):
+        # here, image option is used to
+        # set image on button
+        # compound option is used to align
+        # image on LEFT side of button
+        # buttonss for each child: Nikau, Tia and Hana
+        Button(frame2, text='Nikau', image=nikaupho, compound=LEFT, width=(app_width - 20)).grid(column=0,
+                                                                                                 row=2)
+
+        Button(frame2, text='Hana', image=hanapho, compound=LEFT, width=(app_width - 20)).grid(column=0, row=3)
+        Button(frame2, text='Tia', image=tiapho, compound=LEFT, width=(app_width - 20)).grid(column=0, row=4)
+
         root.title("Allowance app")
         root.configure()
         root.mainloop()
-
-        # puts image in the corner of the page
-        self.logo = PhotoImage(file=r"growth.png")
-        self.logo = self.logo.subsample(7, 7)
-        root.iconphoto(False, self.logo)
-
-        # makes help icon show up on GUI
-        self.help = PhotoImage(file=r"help (1).png")
-        self.help = self.help.subsample(2, 2)
-
-        # adding style to elements on the GUI
-        self.style = ttk.Style()
-        self.style.configure('allowance', background="#f9f9f9ff")
-        self.style.configure('frame1', background="#f9f9f9ff")
-        self.style.configure('frame1_header')
-
-        # Frame 1, encompasses the entire page
-        self.frame1 = ttk.Frame(root)
-        # A separate frame for the logo
-        self.frame1_logo = ttk.Label(self.frame1, image=self.logo)
-        # A separate frame for the header
-        self.frame1_header = ttk.Label(self.frame1, text="Ranui Moni", font=('Comfortaa', 20))
-
-        # A separate frame for the help icon
-        self.frame1.grid(column=2, row=2, sticky="NSEW")
-        self.frame1_logo.grid(column=1, row=0, pady=5, padx=(350 / 2 - 40), sticky="NSEW")
-        self.frame1_header.grid(column=1, row=1, pady=0, padx=(350 / 2 - 80), sticky="NSEW")
-
-    def MainPageButtons(event):
-
-
-    # Creating a photoImage object to use image
-    nikaupho = PhotoImage(file=r"jellyfish.png")
-    hanapho = PhotoImage(file=r"fox.png")
-    tiapho = PhotoImage(file=r"whale.png")
-
-    # Resizing image to fit on button
-    nikaupho = nikaupho.subsample(4, 4)
-    hanapho = hanapho.subsample(4, 4)
-    tiapho = tiapho.subsample(4, 4)
-
-    # here, image option is used to
-    # set image on button
-    # compound option is used to align
-    # image on LEFT side of button
-    # buttons for each child: Nikau, Tia and Hana
-    nikau_button = Button(root, text='Nikau', image=nikaupho,
-           compound=LEFT).pack(side=TOP)
-    tia_button = Button(root, text='Hana', image=hanapho,
-           compound=LEFT).pack(side=TOP)
-    hana_button = Button(root, text='Tia', image=tiapho,
-           compound=LEFT).pack(side=TOP)
-    mainloop()
-
-
 
 
 AllowanceGUI()
